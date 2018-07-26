bdfe5d1d031b9982399991a722f1ba3087a80efd
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-26 15:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
